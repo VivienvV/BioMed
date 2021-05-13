1fd24028baba47a690506ad0857f67811a44fb83
Vivien van Veldhuizen <vivien.van.veldhuizen@hotmail.com>
2021-05-13 14:33:16 +0200

    Completed nested CV

--- model/train.py
+++ model/train.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import pickle
 import argparse
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import TensorDataset, DataLoader, ConcatDataset
@@ -12,20 +14,23 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import KFold, cross_val_score
+from sklearn.feature_selection import SelectKBest, VarianceThreshold
 from sklearn.feature_selection import SelectFromModel 
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.neighbors import NearestCentroid
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from skorch import NeuralNetClassifier
 
 class NN(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, NN_hidden):
         super(NN, self).__init__()
         self.network = nn.ModuleList()
 
         if args.NN_hidden == [0]:
             self.network.append(nn.Linear(args.num_features, args.num_classes))
         else:
-          sizes = [args.num_features] + args.NN_hidden
+          sizes = [args.num_features] + NN_hidden
           for input, output in zip(sizes, sizes[1:]):
               self.network.append(nn.Linear(input, output))
               self.network.append(nn.ReLU())
@@ -60,125 +65,25 @@ def preprocess_data(train_call, train_clin):
     train_arr = train_arr.T
     l = train_clin[train_clin.columns[-1:]].to_numpy().T.squeeze()
     labels = np.array(pd.factorize(l)[0].tolist())
-    clf = ExtraTreesClassifier(n_estimators=50)
-    clf = clf.fit(train_arr, labels)
-    clf.feature_importances_  
-    model = SelectFromModel(clf, prefit=True)
+    return train_arr, labels
+
+def feature_selection(args, X_train, X_test, y_train, y_test, train_call):
+
+    # remove the features beneath a threshold (removes 5 features)
+    sel_variance_threshold = VarianceThreshold(.85 * (1 - .85)) 
+    X_train_remove_variance = sel_variance_threshold.fit_transform(X_train)
+
+    # remove the same features from the testing data
+    X_test_remove_variance = sel_variance_threshold.transform(X_test)
+
+    # select the 900 best features 
+    model = SelectKBest(k=args.num_features)
+    
+    X_train = model.fit_transform(X_train_remove_variance, y_train)
+    X_test = model.transform(X_test_remove_variance)
     cols = model.get_support(indices=True)
-    train_arr = model.transform(train_arr.astype(np.float32))
     
-    return train_arr, labels, full_data.iloc[cols]
-
-def train_and_test(args, model, train_loader, test_loader):
-
-  criterion = nn.CrossEntropyLoss()
-  optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)  
-
-  n_total_steps = len(train_loader)
-  losslist = []
-  acclist = []
-
-  for epoch in range(args.num_epochs):
-      n_correct = 0
-      n_samples = 0
-      tloss = 0
-      # print(train_loader[0])
-      for i, (arrayseq, labels) in enumerate(train_loader):  
-          if args.model == 'LSTM':
-            arrayseq = arrayseq.reshape(-1, args.num_features, 1)
-          arrayseq = arrayseq.to(args.device)
-          labels = labels.to(args.device)
-          
-          # Forward pass
-          outputs = model(arrayseq)
-          loss = criterion(outputs, labels)
-          tloss += loss
-
-          # Backward and optimize
-          optimizer.zero_grad()
-          loss.backward()
-          optimizer.step()
-
-          _, predicted = torch.max(outputs.data, 1)
-          n_samples += labels.size(0)
-          n_correct += (predicted == labels).sum().item()
-          
-          if (i+1) % n_total_steps == 0:
-
-            print (f'\rEpoch [{epoch+1}/{args.num_epochs}], Loss: {loss.item():.4f}, Accuracy: {100.0 * n_correct / n_samples}', end="")
-            
-      accuracy = 100.0 * n_correct / n_samples
-      acclist.append(accuracy)
-      losslist.append(tloss / n_total_steps)
-  print("\nCompleted training!\n")
-  torch.save(model, "model/{}.pth".format(args.model))
-
-  with torch.no_grad():
-      n_correct = 0
-      n_samples = 0
-      for arrayseq, labels in test_loader:
-          if args.model == 'LSTM':
-            arrayseq = arrayseq.reshape(-1, args.num_features, args.input_size)
-          arrayseq = arrayseq.to(args.device)
-          labels = labels.to(args.device)
-          outputs = model(arrayseq)
-          _, predicted = torch.max(outputs.data, 1)
-          n_samples += labels.size(0)
-          n_correct += (predicted == labels).sum().item()
-      print(n_correct, 'correct of ', n_samples)
-      testacc = 100.0 * n_correct / n_samples
-      print(f'Accuracy of the network on the test Array sequences: {testacc} %')
-
-  return acclist, losslist, testacc
-
-def cross_validation(args, model, train_arr, labels):
-
-  dataset =  TensorDataset(torch.from_numpy(train_arr), torch.from_numpy(labels))
-
-  k_folds = 5
-  # For fold results
-  results = {}  
-  # Define the K-fold Cross Validator
-  kfold = KFold(n_splits=k_folds, shuffle=True)
-    
-  # Start print
-  print('--------------------------------')
-
-  # K-fold Cross Validation model evaluation
-  for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
-    
-    # Print
-    print(f'FOLD {fold}')
-    print('--------------------------------')
-    
-    # Sample elements randomly from a given list of ids, no replacement.
-    train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
-    test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
-    
-    # Define data loaders for training and testing data in this fold
-    trainloader = torch.utils.data.DataLoader(
-                      dataset, 
-                      batch_size=args.batch_size, sampler=train_subsampler)
-    testloader = torch.utils.data.DataLoader(
-                      dataset,
-                      batch_size=args.batch_size, sampler=test_subsampler)
-    
-    
-    # Initialize optimizer
-    
-    _,_, acc = train_and_test(args, model, trainloader, testloader)
-    results[fold] = acc
-    
-  # Print fold results
-  print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
-  print('--------------------------------')
-  sum = 0.0
-  for key, value in results.items():
-    print(f'Fold {key}: {value} %')
-    sum += value
-  print(f'Average: {sum/len(results.items())} %')
-
-  return
+    return X_train, X_test, train_call.iloc[cols]
 
 def feature_importance(args, model, X_train, X_test, fdf):
   features = (fdf["ID_no"]).tolist()
@@ -214,7 +119,7 @@ def main():
   # Model params
   parser.add_argument('--model', type=str, default='NN',
                       help='Which model to use for training: NN or LSTM')
-  parser.add_argument('--num_features', type=int, default=2834,
+  parser.add_argument('--num_features', type=int, default=900,
                       help='Length of an input sequence/ amount of features each sample contains')
   parser.add_argument('--input_size', type=int, default=1,
                       help='Size of an input sequence')
@@ -222,7 +127,7 @@ def main():
                       help='Number of units in each LSTM layer')
   parser.add_argument('--LSTM_num_layers', type=int, default=2,
                       help='Number of hidden layers in the LSTM')
-  parser.add_argument('--NN_hidden', type=list, default=[128,128,128],
+  parser.add_argument('--NN_hidden', type=list, default=[128,128],
                       help='List of which the length is the number of hidden layers and the values are the layer sizes in the NN')
   parser.add_argument('--num_classes', type=int, default=3,
                       help='Number of classes the model needs to be able to predict')
@@ -234,6 +139,8 @@ def main():
                       help='Learning rate')
   parser.add_argument('--num_epochs', type=int, default=50,
                       help='Amount of epochs used in training')
+  parser.add_argument('--num_folds', type=int, default=5,
+                      help='Amount of folds for outer CV loop')  
   parser.add_argument('--test_size', type=float, default=0.3,
                       help='Amount of data to use for testing (leave zero to use all data for training')
   parser.add_argument('--device', type=str, default='cuda',
@@ -241,40 +148,99 @@ def main():
 
   args, unknown = parser.parse_known_args()
 
-
-  # Device configuration
-  # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  # Load the data innto PyTorch
-  train_call = pd.read_csv('full_data.csv', delimiter=',' )
-  train_clin = pd.read_csv('Train_clinical.txt', delimiter='\t' )
-  train_arr, labels, new_df = preprocess_data(train_call, train_clin)
-
-  args.num_features = train_arr.shape[1]
-  print("Number of features that will be used: ", args.num_features)
-  if args.model == 'NN':
-    model = NN(args).to(args.device)
-  elif args.model == 'LSTM':
-    model = LSTM(args).to(args.device)
-
   torch.manual_seed(42)
 
+  if args.model == 'NN':
+    model = NN(args, args.NN_hidden).to(args.device)
+    params = {
+        'optimizer__lr' : [0.001, 0.0001, 0.0003],
+        'optimizer__weight_decay' : [0.1, 0.01, 0.001, 0.0001],
+        'max_epochs' : [50, 100, 120],
+        'module__NN_hidden': [[450], [128], [450, 100], [128, 128]],
+    }
+  elif args.model == 'LSTM':
+    model = LSTM(args).to(args.device)
+    params = {
+        'optimizer__lr' : [0.001, 0.0001, 0.0003],
+        'optimizer__weight_decay' : [0.1, 0.01, 0.001, 0.0001],
+        'max_epochs' : [100, 150],
+        'LSTM_hidden_size': [128, 450], 
+        'LSTM_num_layers' : [1, 2]
+    }  
+
+  train_call = pd.read_csv('full_data.csv', delimiter=',' )
+  train_clin = pd.read_csv('Train_clinical.txt', delimiter='\t' )
+  train_arr, labels = preprocess_data(train_call, train_clin)
+
+  net = NeuralNetClassifier(
+    module=model, 
+    criterion=torch.nn.CrossEntropyLoss, 
+    max_epochs=args.num_epochs, 
+    module__args=args,
+    optimizer=optim.Adam, 
+    )
+
+  # START CROSS VALIDATION
+  kf = KFold(n_splits=args.num_folds)
+  i = 1
+  best_params = {}
+  best_scores = {}
+  best = 0
+
+  for train_index, test_index in kf.split(train_arr):
+    print("FOLD", i)
+    X_train, X_test = train_arr[train_index], train_arr[test_index]
+    y_train, y_test = labels[train_index], labels[test_index]
+    X_train, X_test, _ = feature_selection(args, X_train, X_test, y_train, y_test, train_call)
+    net.set_params(train_split=False, verbose=0)
+    gs = GridSearchCV(net, params, refit=True, cv=3, scoring='accuracy', verbose=0)
+    gs.fit(X_train.astype('float32'), y_train.astype('int64'))
+
+    print("best score: {:.3f}, best params: {}\n".format(gs.best_score_, gs.best_params_))
+    
+    best_scores[i] = gs.best_score_
+    best_params[gs.best_score_] = gs.best_params_
+    i += 1
+    
+  # Print fold results
+  print(f'K-FOLD CROSS VALIDATION RESULTS FOR {args.num_folds} FOLDS')
+  print('--------------------------------')
+  sum = 0.0
+  for key, value in best_scores.items():
+    print(f'Fold {key}: {value} %')
+    sum += value
+  print(f'Average: {sum/len(best_scores.items())} %')
+  best_modelparams = best_params[max(best_scores.values())]
+  print('Best params:', best_modelparams)
+
+  # Outer loop: train best model for 10 folds and report mean accuracy 
+  best_model = NeuralNetClassifier(
+    module=model, 
+    module__NN_hidden=best_modelparams['module__NN_hidden'],
+    criterion=torch.nn.CrossEntropyLoss, 
+    max_epochs=best_modelparams['max_epochs'], 
+    module__args=args,
+    optimizer=optim.Adam, 
+    optimizer__lr=best_modelparams['optimizer__lr'],
+    optimizer__weight_decay=best_modelparams['optimizer__weight_decay']
+    )  
+
+  # Save model to pickle file
+  with open('{}.pkl'.format(args.model), 'wb') as f:
+    pickle.dump(best_model, f)
+
   X_train, X_test, y_train, y_test = train_test_split(train_arr, labels, test_size=args.test_size)
-  train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
-  test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
+  X_train, X_test, new_df = feature_selection(args, X_train, X_test, y_train, y_test, train_call)
+  X = np.r_[X_train, X_test].astype('float32')
+  y = np.r_[y_train, y_test].astype('int64')
+  print('Mean accuracy of best performing model:', np.mean(cross_val_score(best_model, X, y, cv=10, scoring='accuracy', verbose=0)))
 
-  train_loader = torch.utils.data.DataLoader(train_data, 
-                                            batch_size=args.batch_size,
-                                            shuffle=True)
-  test_loader = torch.utils.data.DataLoader(test_data, 
-                                            batch_size=args.batch_size, 
-                                            shuffle=False)
 
-  # train_and_test(args, model, train_loader, test_loader)
-  cross_validation(args, model, train_arr, labels)
+  # feature_importance(args, best_model, X_train, X_test, new_df)
 
-  feature_importance(args, model, X_train, X_test, new_df)
-  
+  # # loading
+  # with open('{}.pkl'.format(args.model), 'rb') as f:
+  #     best_model = pickle.load(f) 
 
-  
 if __name__ == '__main__':
     main()
